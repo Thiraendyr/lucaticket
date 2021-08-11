0617a8aa5a1e355e0a99b75d98dba6e0cd83d0f9
David Talavera <davidtb34@hotmail.com>
2021-08-11 10:11:42 +0200

    Servidor Eureka

---
+++ EurekaServer/src/test/java/com/lucaticket/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaticket.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
